b958386fad4b5123a0ad775f7628483b776bfc07
Oleksii Prosiankin <Oleksii.Prosiankin@gov.bc.ca>
2019-10-04 13:20:32 -0700

    Add project files.

---
+++ Beliefs-desires-intentions-Agent-Architecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Beliefs_desires_intentions_Agent_Architecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
